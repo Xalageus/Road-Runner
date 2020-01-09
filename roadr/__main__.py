ce0b0d2eeb9cb422493c3f817491626c2513217e
Xalageus <xalageus@gmail.com>
2020-01-09 02:15:35 -0600

    Support tilesys semi-quiet mode

--- roadr/__main__.py
+++ roadr/__main__.py
@@ -1,9 +1,10 @@
 from roadr.system import system
 
 DEBUG = True
+DEBUG_QUIET_TILE_SYS = True
 
 def main():
-    sys = system(DEBUG)
+    sys = system(DEBUG, DEBUG_QUIET_TILE_SYS)
     sys.mainLoop()
     
 if __name__ == "__main__":
